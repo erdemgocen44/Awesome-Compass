89f729b8421628c7aaf850580fb2e456c85826e1
Erdem <erdemgocen44@gmail.com>
2023-01-20 19:02:02 +0100

    pubspec ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(const Compass());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Compass extends StatelessWidget {
+  const Compass({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Material App',
       home: Scaffold(
         appBar: AppBar(

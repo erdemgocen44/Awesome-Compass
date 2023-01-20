7331557182ef2e41eafd8aa8d28cda55e2f001fd
Erdem <erdemgocen44@gmail.com>
2023-01-20 19:10:01 +0100

    pubspec ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() => runApp(const Compass());
 
-class Compass extends StatelessWidget {
+class Compass extends StatefulWidget {
   const Compass({super.key});
 
   @override
+  State<Compass> createState() => _CompassState();
+}
+
+class _CompassState extends State<Compass> {
+  bool _hasPermissions = false;
+  @override
+  void initState() {
+    super.initState();
+    _fetchPermissionStatus();
+  }
+
+  void _fetchPermissionStatus() {
+    Permission.locationWhenInUse.status.then((status) {
+      setState(() {
+        _hasPermissions == (status == PermissionStatus.granted);
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+      home: Scaffold(),
     );
   }
 }

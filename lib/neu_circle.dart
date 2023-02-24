8da39d48d213fcd15b86494bc93bc19b32a684c1
Erdem <erdemgocen44@gmail.com>
2023-02-24 22:39:54 +0100

    neu circle cree

--- lib/neu_circle.dart
+++ lib/neu_circle.dart
@@ -11,19 +11,23 @@ class NeuCircle extends StatelessWidget {
       alignment: Alignment.center,
       margin: EdgeInsets.all(40),
       decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        color: Colors.blueAccent,
-        boxShadow: BoxShadow(
-            color: Colors.blueAccent.shade700,
-            offset: Offset(4.0, 4.0),
-            blurRadius: 15.0,
-            spreadRadius: 1.0),
-        BoxShadow(
-            color: Colors.blueAccent.shade400,
-            offset: Offset(-4.0, -4.0),
-            blurRadius: 15.0,
-            spreadRadius: 1.0),
-      ),
+          shape: BoxShape.circle,
+          color: Colors.blueAccent,
+          boxShadow: [
+            BoxShadow(
+                color: Colors.blueAccent.shade700,
+                offset: Offset(4.0, 4.0),
+                blurRadius: 15.0,
+                spreadRadius: 1.0),
+            BoxShadow(
+                color: Colors.blueAccent.shade400,
+                offset: Offset(-4.0, -4.0),
+                blurRadius: 15.0,
+                spreadRadius: 1.0),
+          ],
+          gradient: LinearGradient(begin: Alignment.topLeft, end: Alignment.bottomRight,colors: [Colors.blueAccent.shade400])
+          
+          ),
     );
   }
 }
